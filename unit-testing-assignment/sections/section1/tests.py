c93fbe633e7cc136a13c8d4c7238cb47b987797f
Michael Rhode <157053208+rhodemr@users.noreply.github.com>
2025-10-12 21:09:34 -0400

    Update tests.py
    uploaded wrong code before

--- unit-testing-assignment/sections/section1/tests.py
+++ unit-testing-assignment/sections/section1/tests.py
@@ -6,7 +6,7 @@ from my_calculations import Calculations
 class TestCalculations(unittest.TestCase):
 
     def test_sum(self):
-        calculation = Calculations(9, 2)
+        calculation = Calculations(8, 2)
         self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong.')
 
 if __name__ == '__main__':
