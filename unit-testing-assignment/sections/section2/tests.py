66daf4525326195dcf1dd195c3f22a8d0b2163f3
Michael Rhode <157053208+rhodemr@users.noreply.github.com>
2025-10-12 21:08:23 -0400

    Update and rename Unittest to tests.py

---
+++ unit-testing-assignment/sections/section2/tests.py
@@ -0,0 +1,13 @@
+# project/tests.py
+
+import unittest
+from my_calculations import Calculations
+
+class TestCalculations(unittest.TestCase):
+
+    def test_sum(self):
+        calculation = Calculations(9, 2)
+        self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong.')
+
+if __name__ == '__main__':
+    unittest.main()
